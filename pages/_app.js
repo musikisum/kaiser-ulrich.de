67cdb0b5e6eca45f9884c98608bde3c2c0c5154a
Ulrich Kaiser <ulrich.kaiser@hmtm.de>
2023-08-02 17:40:08 +0200

    Test chakra ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
 import '../styles/globals.css'
 import 'semantic-ui-css/semantic.min.css'
+import { ChakraProvider } from '@chakra-ui/react'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
