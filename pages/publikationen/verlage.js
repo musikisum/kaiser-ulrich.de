c1d4c5e18c926df2563f12f3efb8fdd50ce5ab5e
musikisum <ulrich.kaiser@hmtm.de>
2023-09-17 09:54:48 +0200

    Prepare table of  pubisher site

--- pages/publikationen/verlage.js
+++ pages/publikationen/verlage.js
@@ -1,15 +1,23 @@
-import Head from 'next/head'
-import ShortUniqueId from 'short-unique-id'
+import Head from 'next/head';
+import Layout from "../components/layout";
+import PageHeader from '../components/pagehaeder';
+import ShortUniqueId from 'short-unique-id';
+import { Table, Thead, Tbody, Tfoot, Tr, Th, Td, TableCaption, TableContainer } from '@chakra-ui/react'
 
 import style from './index.module.css'
 
-import books from '../../data/books.json'
+import books from '../../data/verlage.json'
 
 const uid = new ShortUniqueId()
-const oer = books.filter(book => book.type === 'OER')
-const publisher = books.filter(book => book.type === 'publisher') 
 
-export default function Verlage() {
+const options = {
+  title: 'Bücher & Hefte',
+  description: 'Hier finden Sie selbstständige Publikationen, die in den Verlagen Bärenreiter und Klett in den Jahren von 1998 bis 2007 veröffentlicht worden sind.',
+  filter: 'publikationen',
+  slug: '/publikationen/verlage'
+}
+
+const Verlage = () => {
   return (
     <>
       <Head>
@@ -18,7 +26,31 @@ export default function Verlage() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/images/icon.png" />
       </Head>
-      <div>Verlage</div>
+      <PageHeader options={ options } />
+      <TableContainer>
+        <Table variant='simple'>
+          <TableCaption>Selbstständige Verlagspublikationen von Ulrich Kaiser</TableCaption>
+          <Thead>
+            <Tr>
+              <Th>Abbildung</Th>
+              <Th>Bibliographische Angabe</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+           
+          </Tbody>
+        </Table>
+      </TableContainer>
     </>
   )
 }
+
+Verlage.getLayout = function getLayout(page) {
+  return (
+    <Layout text='Prof. Dr. Ulrich Kaiser – OER / Musiktheorie / Multimedia'>
+      {page}
+    </Layout>
+  )
+}
+
+export default Verlage;
