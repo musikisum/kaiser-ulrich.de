4fab9a9b78293342a6d367c5ba1c0efeb67f3f86
Ulrich Kaiser <ulrich.kaiser@hmtm.de>
2023-09-20 01:56:00 +0200

    Restore menu as menulist

--- pages/components/navigation.js
+++ pages/components/navigation.js
@@ -1,11 +1,11 @@
 
 import ShortUniqueId from 'short-unique-id';
 import { ChevronDownIcon } from '@chakra-ui/icons';
-import { Menu, MenuButton, MenuList, MenuItem, MenuDivider, Button } from '@chakra-ui/react';
-
+import { Menu, MenuButton, MenuList, MenuItem, MenuDivider, Button, Breadcrumb, BreadcrumbItem, BreadcrumbLink, Text } from '@chakra-ui/react';
 
 import sitemap from "../../data/sitemap.json";
 import sitemapDictionary from "../../data/sitemapDictionary.json";
+
 import style from './navigation.module.css';
 
 const uid = new ShortUniqueId();
@@ -26,14 +26,15 @@ function getUrls(filter) {
 }
 
 export default function Navigation({ filter, slug }) {
+  const home = '/';
   const urls = getUrls(filter);
   const theme = urls[0];
   const hasTheme = theme !== slug;
-  const themeLinks = urls.slice(1).filter(url => url !== slug);
+  const themeLinks = urls.slice(1);
 
   return (
     <Menu>
-      <MenuButton as={ Button } rightIcon={<ChevronDownIcon />} >Andere Publikationsform?</MenuButton>
+      <MenuButton as={ Button } rightIcon={<ChevronDownIcon />} >Andere Auswahl?</MenuButton>
       <MenuList>
         { hasTheme && <MenuItem ml='4px' as='a' href={theme}><b>{sitemapDictionary[theme]}</b></MenuItem> }
         { hasTheme && <MenuDivider /> }        
@@ -46,5 +47,20 @@ export default function Navigation({ filter, slug }) {
         <MenuItem ml='4px' as='a' href='/'><b>Home</b></MenuItem>
       </MenuList>
     </Menu>
+    // <Breadcrumb>
+    //   <BreadcrumbItem><BreadcrumbLink href={home}>Home</BreadcrumbLink></BreadcrumbItem>
+    //   {
+    //     theme !== slug ?
+    //     <BreadcrumbItem><BreadcrumbLink href={theme}>{sitemapDictionary[theme]}</BreadcrumbLink></BreadcrumbItem> : 
+    //     <BreadcrumbItem isCurrentPage><BreadcrumbLink href={theme}><Text color='green'>{sitemapDictionary[theme]}</Text></BreadcrumbLink></BreadcrumbItem>
+    //   }
+    //   {
+    //     themeLinks.map(url => {
+    //       return url !== slug ? 
+    //       <BreadcrumbItem key={uid.seq()}><BreadcrumbLink href={url}>{ sitemapDictionary[url] }</BreadcrumbLink></BreadcrumbItem> :
+    //       <BreadcrumbItem isCurrentPage key={uid.seq()}><BreadcrumbLink href={url}><Text color='green'>{sitemapDictionary[url]}</Text></BreadcrumbLink></BreadcrumbItem>
+    //     })
+    //   }
+    // </Breadcrumb>
   )
 }
