572f6231a35f38d3e03a59895dc076cf33077ddb
Ulrich Kaiser <ulrich.kaiser@hmtm.de>
2024-02-14 00:58:57 +0100

    Update next and jslint

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ const Home = () => {
         <meta name="description" content="Homepage von Prof. Dr. Ulrich Kaiser" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <HomeImage />   
+      <HomeImage />
     </>
   )
 }
